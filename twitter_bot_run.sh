e23de72c69bcd52e2cc675204b230de3b13ae988
yossarian1453 <61842330+yossarian1453@users.noreply.github.com>
2021-04-02 15:10:42 -0500

    Update twitter_bot_run.sh

--- twitter_bot_run.sh
+++ twitter_bot_run.sh
@@ -2,10 +2,10 @@
 # -x to print commands before executing them
 set -x
 # stage 1
-/usr/local/bin/python3 /Users/robertness/twitter_bot_alert/stage_1/tweet_pull.py
-#/usr/local/bin/python3 /Users/robertness/twitter_bot_alert/stage_1/retweet.py
+/usr/local/bin/python3 /locataion/of/repo/twitter_bot_alert/stage_1/tweet_pull.py
+/usr/local/bin/python3 /locataion/of/repo/twitter_bot_alert/stage_1/retweet.py
 
 # stage 2
-/usr/local/bin/python3 /Users/robertness/twitter_bot_alert/stage_2/double_quote_removal.py
-/usr/local/bin/python3 /Users/robertness/twitter_bot_alert/stage_2/tweet_loader_pg.py
-/usr/local/bin/python3 /Users/robertness/twitter_bot_alert/stage_2/file_clear.py
+/usr/local/bin/python3 /locataion/of/repo/twitter_bot_alert/stage_2/double_quote_removal.py
+/usr/local/bin/python3 /locataion/of/repo/twitter_bot_alert/stage_2/tweet_loader_pg.py
+/usr/local/bin/python3 /locataion/of/repo/twitter_bot_alert/stage_2/file_clear.py
