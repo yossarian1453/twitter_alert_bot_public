6bd7d3fa2ff99eb39576ea56220cdc1cd387ec7f
yossarian1453 <61842330+yossarian1453@users.noreply.github.com>
2021-04-02 15:02:03 -0500

    Update tweet_loader_pg.py

--- stage_2/tweet_loader_pg.py
+++ stage_2/tweet_loader_pg.py
@@ -3,7 +3,7 @@ import os
 
 file_path = os.getcwd()
 
-connection = psycopg2.connect("dbname=twitter_bot user=twitter_bot password=SkynetJudgement1992")
+connection = psycopg2.connect("dbname=the_database_name user=the_user_you_use password=your_postgres_user_pass")
 cursor = connection.cursor()
 # cursor.execute("DROP TABLE IF EXISTS analysis.tweet_collection")
 cursor.execute(f"""
