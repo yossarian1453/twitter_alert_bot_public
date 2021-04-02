1473a21dc9aa2a98de4f0abd34b1831358133a3a
yossarian1453 <61842330+yossarian1453@users.noreply.github.com>
2021-04-02 15:09:52 -0500

    Update twitter_bot_run.py

--- twitter_bot_run.py
+++ twitter_bot_run.py
@@ -1,13 +1,13 @@
 import os
 
 # stage 1
-tweet_pull = '/usr/local/bin/python3 /Users/robertness/twitter_bot_alert/stage_1/tweet_pull.py'
-retweet = '/usr/local/bin/python3 /Users/robertness/twitter_bot_alert/stage_1/retweet.py'
+tweet_pull = '/usr/local/bin/python3 /locataion/of/repo/twitter_bot_alert/stage_1/tweet_pull.py'
+retweet = '/usr/local/bin/python3 /locataion/of/repo/twitter_bot_alert/stage_1/retweet.py'
 
 # stage 2
-double_quote_removal = '/usr/local/bin/python3 /Users/robertness/twitter_bot_alert/stage_2/double_quote_removal.py'
-tweet_loader_pg = '/usr/local/bin/python3 /Users/robertness/twitter_bot_alert/stage_2/tweet_loader_pg.py'
-file_clear = '/usr/local/bin/python3 /Users/robertness/twitter_bot_alert/stage_2/file_clear.py'
+double_quote_removal = '/usr/local/bin/python3 /locataion/of/repo/twitter_bot_alert/stage_2/double_quote_removal.py'
+tweet_loader_pg = '/usr/local/bin/python3 /locataion/of/repo/twitter_bot_alert/stage_2/tweet_loader_pg.py'
+file_clear = '/usr/local/bin/python3 /locataion/of/repo/twitter_bot_alert/stage_2/file_clear.py'
 
 # stage 1
 os.system(tweet_pull)
