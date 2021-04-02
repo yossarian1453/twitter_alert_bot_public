af5bf2a78e0efa5cc8aa14e60fabeb13de16aabb
yossarian1453 <61842330+yossarian1453@users.noreply.github.com>
2021-04-02 15:07:08 -0500

    Update user_id_pull.py

--- spare_parts/user_id_pull.py
+++ spare_parts/user_id_pull.py
@@ -6,6 +6,8 @@ import subprocess
 twitter_users = pd.read_csv('/Users/robertness/Documents/Civitech/Python/twitter_alert_bot/twitter_handles.csv')
 test_csv = '/Users/robertness/Documents/Civitech/Python/twitter_alert_bot/test.csv'
 
+bearer_token = 'INSERT BEARER TOKEN HERE'
+
 # select the column to pull
 user_name = twitter_users['twitter_account']
 
@@ -17,7 +19,7 @@ for i in range (len(user_name)):
     #print(directory)
     handle = user.split('/',3)
     last_portion = handle[3]
-    command = f'curl "https://api.twitter.com/2/users/by/username/{last_portion}" -H "Authorization: Bearer AAAAAAAAAAAAAAAAAAAAAE2cMgEAAAAA%2F09FeHOkK5mqers%2Fa32RLBOVkVk%3DFza0CBgM4qKFx81HDe28W6dJUHaimXCESmqYFEtmx2lRvtRfYK"'
+    command = f'curl "https://api.twitter.com/2/users/by/username/{last_portion}" -H "Authorization: Bearer {bearer_token}"'
     # def append_list_as_row(file_name, list_of_elem):
     with open(test_csv, 'a+', newline='') as wf:
         pulled_json = subprocess.check_output(command, shell=True)
